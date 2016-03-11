50bcec82caaec03d0450ce29758dfd3668f7673c
subins2000 <subins2000@gmail.com>
2016-03-11 16:58:34 +0530

    New column

--- tables.sql
+++ tables.sql
@@ -185,6 +185,7 @@ CREATE TABLE IF NOT EXISTS `trend` (
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` text NOT NULL,
+  `email` varchar(100) NOT NULL,
   `password` text NOT NULL,
   `psalt` text NOT NULL,
   `name` varchar(100) NOT NULL,
